5ed4233aaf01d80f1a743c29e22cdd1543b7e177
juliaem <juliaem@gmail.com>
2016-12-21 10:57:51 -0500

    Added Faithful syntax exercise

---
+++ faithful.R
@@ -0,0 +1,25 @@
+#Faithful Exercise
+
+#Open dataset
+
+View(faithful)
+
+#Understand data structure
+
+str(faithful)
+
+#Calculate summary statistics
+
+min(faithful$waiting)
+max(faithful$waiting)
+mean(faithful$waiting)
+median(faithful$waiting)
+
+#Visualize data
+
+hist(faithful$waiting)
+hist(faithful$eruptions)
+
+attach(faithful)
+plot(eruptions, waiting)
+abline(lm(waiting~eruptions), col="red")
