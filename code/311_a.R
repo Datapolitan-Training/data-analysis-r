a6fcf44d9dc1c54796a4bab4a6f72e9a5ed0c628
Richard Dunks <richard@datapolitan.com>
2020-03-09 15:23:48 -0500

    311a code cleanup

--- code/311_a.R
+++ code/311_a.R
@@ -4,11 +4,11 @@
 # Use ? if you get lost, ask a neighbor, or raise your hand
 
 # LOAD DATASET
-#key command: read.csv()
+# key command: read.csv()
 
 mydata <- read.csv("https://s3.amazonaws.com/datapolitan-training-files/311_Requests_Oct15_Nov20.csv", header=TRUE, sep=",")
 
-#Explore dataset
+# Explore dataset
 
 dim(mydata)
 colnames(mydata)
@@ -17,21 +17,21 @@ head(mydata[,c(2,6,7,8,9,24,25)])
 summary(mydata)
 summary(mydata[,c(2,6,7,8,9,24,25)])
 
-#Understand data structure
-#Identify the class of Borough, Complaint.Type, Incident Zip, and Created.Date
+# Understand data structure
+# Identify the class of Borough, Complaint.Type, Incident Zip, and Created.Date
 
 str(mydata[,c(2,6,7,8,9,24,25)])
 class(mydata$Borough)
 class(mydata$INSERTCOLUMNNAME)
 
-#Calculate summary statistics for 1-2 of the above columns
+# Calculate summary statistics for 1-2 of the above columns
 
 min(mydata$Borough)
 max(mydata$INSERTCOLUMNNAME)
 mean(mydata$INSERTCOLUMNNAME)
 median(mydata$INSERTCOLUMNNAME)
 
-#What happens when you calculate these statistics? Why do you think that is?
+# What happens when you calculate these statistics? Why do you think that is?
 
 summary(mydata$Borough)
 table(mydata$Borough)
@@ -39,12 +39,12 @@ table(mydata$Borough)
 # What's the difference between summary & table? What are these results showing?  
 # Pivot tables 
 
-#Visualize data
+# Visualize data
 
 hist(mydata$Borough)
 hist(mydata$Incident.Zip)
 
-#What's going on here? Why do you think that is? 
+# What's going on here? Why do you think that is? 
 
 hist(table(mydata$Borough))
 
